59be4d7ea731238456090135fe4533588b6f1701
Jaip <j.illanes.pareja@gmail.com>
2024-08-30 11:17:13 -0400

    Add module 3 session 4 drill

---
+++ module_3/4/ejercicio.py
@@ -0,0 +1,8 @@
+
+personal_information_1 = {"name": "Juan", "last_name": "Perez", "phone": 98765432}
+personal_information_2 = {"name": "Pedro", "last_name": "Pereira", "phone": 11223344}
+personal_information_3 = {"name": "María", "last_name": "Magdalena", "phone": 18283719}
+
+lista_personal_information = [personal_information_1, personal_information_2, personal_information_3]
+
+print(lista_personal_information)
